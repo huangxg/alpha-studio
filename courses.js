e23ababfe9f09d506bc9f03855d465d20bf42e40
huangxg <huang.xingang@gmail.com>
2020-06-05 22:05:41 -0700

    update courses list

--- courses.js
+++ courses.js
@@ -3,43 +3,31 @@ var bao = bao || {};
 bao.studio.courses = (() => {
   var list = [
     {
-      title : "Introduction to HTML",
+      id   : "101",
+      name : "Introduction to HTML",
+      url  : "hnotes/index.html",
+      logo : "img/html5.svg",
+      hours: 8
+    }, {
+      id   : "102",
+      name : "Introduction to CSS",
+      url  : "hnotes/index.html",
+      logo : "img/css3.svg",
+      hours: 8
+    }, {
+      id  : "201",
+      name : "Introduction to JavaScript",
       logo  : "img/html5.svg",
-      code  : "101",
       hours : 8
     }, {
-      title : "Introduction to CSS",
-      logo  : "img/css3.svg",
-      code  : "102",
-      hours : 8
-    }, {
-      title : "Introduction to JavaScript",
+      id  : "202",
+      name : "Intermidate JavaScript",
       logo  : "img/html5.svg",
-      code  : "201",
-      hours : 8
-    }, {
-      title : "Intermidate JavaScript",
-      logo  : "img/html5.svg",
-      code  : "202",
       hours : 8
     }
   ];
 
-  function initVue() {
-    Vue.component('course-row', {
-      props: ['course'],
-      template: '<tr>' + 
-                  '<td class="logo-small"><img :src="course.logo"></td>' + 
-                  '<td>{{ course.code }}</td>' +
-                  '<td>{{ course.title }}</td>' + 
-                  '<td>{{ course.hours }}</td>' + 
-                '</tr>'
-    });
-
-  }
-
   return {
-    initVue : initVue,
-    list    : list
+    list : list
   };
 })();
