358674f1e63003da9caf570b846d06acd5cd8e3e
huangxg <huang.xingang@gmail.com>
2020-10-24 09:44:05 -0700

    fix this.js in chapter objects

--- jsnotes/objects/this.js
+++ jsnotes/objects/this.js
@@ -2,7 +2,7 @@ let cat = {
   name: 'Ginger',
   age: 3,
   skills: ['sleeping', 'being cute'],
-  introduction: () => {
+  introduction: function() {
     let skillStr = this.skills.join(' and ');
     return `I am ${this.name}, ${this.age} years old. I am good at ${skillStr}`;
   },
