44dd8804019293efd22c4bdf3aa9531f2ba431a6
huangxg <huang.xingang@gmail.com>
2021-07-10 15:33:53 -0700

    add slice in chp Arrays

--- jsnotes/arrays/access.js
+++ jsnotes/arrays/access.js
@@ -1,5 +1,8 @@
-let avengers = ['Captain America', 'Ironman', 'Wonder Woman'];
+let avengers = ['Captain America', 'Hulk', 'Iron Man', 'Wonder Woman'];
 avengers[0];
 avengers[avengers.length - 1];
-avengers[2] = 'Spiderman';
+avengers[3] = 'Spiderman';
 avengers;
+
+avengers.slice(1);
+avengers.slice(1, 3);
